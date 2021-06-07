0c16ba3499bda29d9d1563fabd217351bc169afb
upwon <1244753906@qq.com>
2021-06-07 17:28:48 +0800

    Update ThreadPool.c

--- ThreadPool.c
+++ ThreadPool.c
@@ -16,7 +16,9 @@
 
 #include "ThreadPool.h"
 #include<pthread.h>
-
+#include<stdbool.h>
+#include <string.h>
+#include <stdio.h>
 // 任务结构体
 typedef struct Task
 {
@@ -47,14 +49,84 @@ struct ThreadPool
 
     // 互斥锁
     pthread_mutex_t mutexPool; //锁整个线程
-    Pthread_mutex_t mutexBusy; //专用于锁busyNumThreads
+    pthread_mutex_t mutexBusy; //专用于锁busyNumThreads
 
     // 条件变量
     pthread_cond_t notFull; // 任务队列是不是满了
     pthread_cond_t notEmpty; // 任务队列是不是空了
 
-    
+
     // 销毁线程标志
     bool shutdownThreadPool; //true表示销毁线程
 
 };
+
+ThreadPool *CreateThreadPool(int minNumThreads_, int maxNumThreads_, int queueSize_)
+{
+
+    ThreadPool *pool = (ThreadPool *) malloc(sizeof(ThreadPool));
+
+
+    do
+    {
+        if (pool == NULL)
+        {
+            printf("malloc threadpool failure \n");
+            break;
+        }
+
+        pool->threadIDs = (pthread_t *) malloc(sizeof(pthread_t) * maxNumThreads_);  // 类型 乘以 数量
+        if (pool->threadIDs == NULL)
+        {
+            printf("malloc threadIDs failure \n");
+            break;
+        }
+        memset(pool->threadIDs, 0, sizeof(pool->threadIDs) * maxNumThreads_); // 初始化
+
+
+        pool->minNumThreads = minNumThreads_;
+        pool->maxNumThreads = maxNumThreads_;
+        pool->busyNumThreads = 0;
+        pool->liveNumThreads = minNumThreads_; // 与最小个数相等
+        pool->destroyNumThreads = 0;
+
+        // 初始化互斥锁与条件量
+        if (pthrad_mutex_ini(&pool->mutexpool,NULL) != 0 || pthread_mutex_init(&pool->mutexBusy, NULL) != 0 ||
+            pthread_cond_init(&pool->notFull, NULL) != 0 || pthread_cond_init(&pool->notFull, NULL) != 0)
+        {
+            printf("mutex or pthread_cond  initialization failure \n");
+            break;
+        }
+
+        // 初始化任务队列
+        pool->taskQueue =(taskQueue*) malloc(sizeof(Task) * queueSize_);
+        pool->queueCapacity = queueSize_;
+        pool->queueSize = queueSize_;
+        pool->queueFront = 0;
+        pool->queueRear = 0;
+
+        pool->shutdownThreadPool = false; // 默认不销毁
+
+
+        // 创建线程
+        pthread_create(&pool->threadManagerID, NULL, manager, NULL);    // 创建管理者线程
+        for (int i = 0; i < pool->minNumThreads; i++)   // 创建工作线程
+        {
+            pthread_create(&pool->threadIDs[i], NULL, worker, NULL);
+        }
+
+    } while (0);
+
+    // 释放资源
+    if (pool && pool->threadIDs)
+    {
+        free(pool->threadIDs);
+    }
+    if(pool&&pool->taskQueue)
+    {
+        free(pool->taskQueue);
+    }
+
+
+
+}
