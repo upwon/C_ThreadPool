accabe772b34b718ba6a712cdc7f93bf9c1332d6
upwon <1244753906@qq.com>
2021-06-08 10:17:33 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <pthread.h>
+#include"ThreadPool.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+void taskFunc(void *arg)
+{
+    int num=*(int*)arg; //转换后取值
+
+    printf("thread %ld is working, number= %d",pthread_self(),num);
+
+    sleep(1);
+}
 
 int main()
 {
+
+    // 创建线程池
+    ThreadPool *pool = CreateThreadPool(3, 10, 100);
+
+    for (int i = 0; i < 100; i++)
+    {
+        int* num=(int*)malloc(sizeof(int));
+        *num=i+100;
+        threadPoolAdd(pool,taskFunc,num);
+    }
+
+
+    sleep(30);  // 睡眠30秒
+    threadPoolDestroy(pool);
+
 
     printf("Hello, World!\n");
     return 0;
